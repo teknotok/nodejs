2c68f78044b1c8d60e98be1de7109cb0b45d3008
teknotok <45201799+teknotok@users.noreply.github.com>
2021-07-31 03:39:25 +0300

    start

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-import {randomLcg, randomNumber} from "d3-random";
-
-const seed = 0.44871573888282423; // any number in [0, 1)
-const random = randomNormal.source(randomLcg(seed))(0, 1);
-
-random(); // -0.6253955998897069
-console.log(random())
+var name = "Selim cadoa";
+console.log(name);
